c7db05bc85dcc106d432d3b6ab56ee7c1dd2fb00
vincent <vincent.arteaga@gmail.com>
2021-03-16 22:25:02 -0700

    commit tree 70f65428380a5faae5709b18d3a6de527b0a9fed

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+// 1) is an equation whose multiplied results will equal 62
+let num1 = 1
+let num2 = 62
+let multiply = num1 * num2
+
+// 3) is an equation that calculates a remainder that is equal to 4
+let num3 = 3.5
+let num4 = 14
+let mod = num4 / num3
+
+// 2) generates a random integer greater than 0 
+let random = Math.floor(Math.random() + 1 )
+
+// 4) will return 20 as the highest number in the set
+
+let max = Math.max(1, 20, 2)
